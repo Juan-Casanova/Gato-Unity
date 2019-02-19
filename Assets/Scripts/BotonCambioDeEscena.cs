1e5fe5e94ba3983c255c35db69f755b15d1dd6a9
Juan-Casanova <casanovajm199621@gmail.com>
2019-02-18 21:50:35 -0600

    Se agrego la imagen del menu boton. Falta programacion del mismo boton

---
+++ Assets/Scripts/BotonCambioDeEscena.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonCambioDeEscena : MonoBehaviour
+{
+    //Cerrar aplicacion
+    public void BotonSalir(){
+        Application.Quit();
+    }
+
+    //inicia la nueva escena
+    public void BotonPlay(){
+        SceneManager.LoadScene("Juego");
+    }
+}
